a64d8f199e06eafdbdca3a5b7e96bfc4db37e8a5
Tim Statler <62028706+timstats91@users.noreply.github.com>
2022-07-07 16:20:06 -0500

    update home link on success page

--- components/SuccessComponent.vue
+++ components/SuccessComponent.vue
@@ -3,7 +3,7 @@
         <div class="container bg-white">
             <h2>Form Successfully Submitted!</h2>
             <p>Return to the home page</p>
-            <n-link to="/">
+            <n-link to="/#hero">
                 <p class="btn btn-success btn-hover-dark read-more-btn">Back</p>
             </n-link>
         </div>
