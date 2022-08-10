df1833903d827b5f6c882f5f42dfc3748a892606
Tim Statler <62028706+timstats91@users.noreply.github.com>
2022-08-10 11:48:27 -0500

    netlify form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -12,54 +12,55 @@
                 <div class="boxed-inner">
                     <!-- Contact Form -->
                     <div class="contact-form">
-                        <form action="https://formsubmit.co/0eb24e92fc02b5f5836b63922260a72f" method="POST">
+                        <form action="POST" name="contact" data-netlify="true">
+                        <!-- <form action="https://formsubmit.co/0eb24e92fc02b5f5836b63922260a72f" method="POST"> -->
+                        
                         <!-- <form> -->
                             
                             <!-- Honeypot -->
-                            <input type="text" name="_honey" style="display: none;">
+                            <!-- <input type="text" name="_honey" style="display: none;"> -->
 
                             <!-- Disable Captcha -->
-                            <input type="hidden" name="_captcha" value="false">
+                            <!-- <input type="hidden" name="_captcha" value="false"> -->
 
                             <!-- Subject -->
-                            <input type="hidden" name="_subject" value="Chicago EV - New Inquiry">
+                            <!-- <input type="hidden" name="_subject" value="Chicago EV - New Inquiry"> -->
 
                             <!-- CC: Additional recipients of the contact form -->
-                            <input type="hidden" name="_cc" value="timstats91@gmail.com">
+                            <!-- <input type="hidden" name="_cc" value="timstats91@gmail.com"> -->
 
                             <!-- Template Style 2: Table -->
-                            <input type="hidden" name="_template" value="table">
+                            <!-- <input type="hidden" name="_template" value="table"> -->
 
                             <!-- Next Page -->
-                            <input type="hidden" name="_next" value="https://ev-chicago.herokuapp.com/success">
+                            <!-- <input type="hidden" name="_next" value="https://ev-chicago.herokuapp.com/success"> -->
 
-                            <!-- Auto Response (Doesn't work unless Captcha is active) -->
-                            <!-- <input type="hidden" name="_autoresponse" value="Thanks for contacting Chicago EV! We'll review the information you included and get back to you within 1-2 business days."> -->
+                            <input type="hidden" name="form-name" value="contact" />
 
                             <div class="row">
                                 <div class="col-lg-4 col-md-6 form-group">
                                     <label for="cars">Full Name*</label>
-                                    <input class="input-item" type="text" placeholder="John Doe" name="Name" required>
+                                    <input class="input-item" type="text" name="Name" required>
                                 </div>
 
                                 <div class="col-lg-4 col-md-6 form-group">
                                     <label for="cars">Email*</label>
-                                    <input class="input-item" type="email" placeholder="johndoe@email.com" name="Email" required>
+                                    <input class="input-item" type="email" name="Email" required>
                                 </div>
 
                                 <div class="col-lg-4 col-md-12 form-group">
                                     <label for="cars">Phone Number</label>
-                                    <input class="input-item" type="tel" placeholder="7735551234" name="Phone Number">
+                                    <input class="input-item" type="tel" name="Phone Number">
                                 </div>
 
                                 <div class="col-lg-4 col-md-6 form-group">
                                     <label for="cars">Company Name</label>
-                                    <input class="input-item" type="text" placeholder="ABC Corp" name="Company Name">
+                                    <input class="input-item" type="text" name="Company Name">
                                 </div>
 
                                 <div class="col-lg-4 col-md-6 form-group">
                                     <label for="cars">Company Address</label>
-                                    <input class="input-item" type="text" placeholder="123 Main St. Chicago, IL" name="Company Address">
+                                    <input class="input-item" type="text" name="Company Address">
                                 </div>
 
                                 <div class="col-lg-4 col-md-12 form-group">
@@ -92,8 +93,12 @@
                                 <div class="col-lg-12 form-group mb-10">
                                     <label for="cars">Message*</label>
                                     <div class="input-item">
-                                        <textarea class="textarea-item" name="Message" placeholder="I need XYZ services for my parking garage..." required></textarea>
+                                        <textarea class="textarea-item" name="Message" required></textarea>
                                     </div>
+                                </div>
+
+                                <div class="field">
+                                    <div data-netlify-recaptcha="true"></div>
                                 </div>
 
                                 <div class="col-lg-12 text-center form-group">
